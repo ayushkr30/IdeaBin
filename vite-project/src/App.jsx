34af01bf02efc759e0a3f1790ac26eb953a25116
Ayush Kumar <ayushpal2017ak@gmail.com>
2024-10-12 13:00:41 +0530

    project setup

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomeScreen from './Screens/Home/HomeScreen'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h2>Subscribe</h2>
+      <HomeScreen />
     </div>
   )
 }
